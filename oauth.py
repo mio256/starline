4ae0df39e30c643254c0584ca5bf64c9c9ceeabb
mio256 <reiya@woorth.io>
2023-01-31 16:52:32 +0900

    :fire:

--- oauth.py
+++ oauth.py
@@ -11,7 +11,7 @@ def login():
     API_KEY = os.environ['CONSUMER_KEY']
     API_KEY_SECRET = os.environ['CONSUMER_SECRET']
 
-    callback_url = "http://127.0.0.1:8000/oauth/"
+    callback_url = "http://localhost:8000/timeline/oauth/"
     request_endpoint_url = "https://api.twitter.com/oauth/request_token"
     authenticate_url = "https://api.twitter.com/oauth/authenticate"
 
@@ -19,7 +19,8 @@ def login():
     response_req = session_req.post(request_endpoint_url, params={"oauth_callback": callback_url})
     response_req_text = response_req.text
 
-    token_dict = url_split_dict(response_req_text)
+    oauth_token_kvstr = response_req_text.split("&")
+    token_dict = {x.split("=")[0]: x.split("=")[1] for x in oauth_token_kvstr}
     oauth_token = token_dict["oauth_token"]
 
     print(f'{authenticate_url}?oauth_token={oauth_token}')
